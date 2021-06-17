fd53cccfff9b650d26ed401d957247b1656ea120
Thomas Eriksson <thomas.anthony.eriksson@gmail,com>
2021-06-17 21:51:39 +0200

    Added layout pages for host and viewer. Added function to join and create room buttons.

--- app/src/main/java/com/thomas/ttrpgimageshare/MainActivity.java
+++ app/src/main/java/com/thomas/ttrpgimageshare/MainActivity.java
@@ -2,7 +2,10 @@ package com.thomas.ttrpgimageshare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         if (getSupportActionBar() != null);
             getSupportActionBar().hide();
+        View.OnClickListener createRoomClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(MainActivity.this, HostActivity.class);
+                startActivity(startIntent);
+            }
+
+        };
+        Button createRoomButton = findViewById(R.id.buttonCreate);
+        createRoomButton.setOnClickListener(createRoomClickListener);
+
+        View.OnClickListener joinRoomClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(MainActivity.this, ViewerActivity.class);
+                startActivity(startIntent);
+            }
+
+        };
+        Button joinRoomButton = findViewById(R.id.buttonJoin);
+        joinRoomButton.setOnClickListener(joinRoomClickListener);
     }
 }
---
+++ app/src/main/java/com/thomas/ttrpgimageshare/ViewerActivity.java
@@ -0,0 +1,2 @@
+package com.thomas.ttrpgimageshare;public class ViewerActivity {
+}
