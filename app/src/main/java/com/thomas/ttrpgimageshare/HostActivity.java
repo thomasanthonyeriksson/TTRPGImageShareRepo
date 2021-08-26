d19ad7c2e0205c8109c29869a43e29e84d94504b
Thomas Eriksson <thomas.anthony.eriksson@gmail,com>
2021-08-26 23:55:10 +0200

    Added upstream for image to server. Malfunctions since server does not accept images.

--- app/src/main/java/com/thomas/ttrpgimageshare/HostActivity.java
+++ app/src/main/java/com/thomas/ttrpgimageshare/HostActivity.java
@@ -3,14 +3,18 @@ package com.thomas.ttrpgimageshare;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,6 +23,7 @@ import android.widget.Adapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
@@ -30,10 +35,21 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 
+import org.apache.http.params.HttpParams;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class HostActivity extends AppCompatActivity {
     int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 940424;
@@ -94,6 +110,80 @@ public class HostActivity extends AppCompatActivity {
         }
     }
 
+    static public class ImageUploader extends AsyncTask<Void, Void, Void>{
+        Context inContext;
+        String inRoom;
+        String inPath;
+
+        ImageUploader(Context context, String room, String path) {
+            inContext = context;
+            inRoom = room;
+            inPath = path;
+        }
+        @Override
+        protected Void doInBackground(Void... voids) {
+            ByteArrayOutputStream byteOutStream = new ByteArrayOutputStream(4096);
+            byte[] data;
+            try {
+                InputStream input = new FileInputStream(inPath);
+                int byteReads;
+                while ((byteReads = input.read()) != -1) {
+                    byteOutStream.write(byteReads);
+                }
+                data = byteOutStream.toByteArray();
+                byteOutStream.close();
+                input.close();
+            } catch (IOException exception) {
+                exception.printStackTrace();
+                return null;
+            }
+
+            String encodedData = Base64.encodeToString(byteOutStream.toByteArray(), Base64.DEFAULT);
+
+            HttpURLConnection connection = null;
+            try {
+                URL url = new URL("http://thomaseriksson.infinityfreeapp.com/DownloadPicture.php");
+
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setDoOutput(true);
+                connection.setDoInput(true);
+                connection.setReadTimeout(30 * 1000);
+                connection.setConnectTimeout(30 * 1000);
+                connection.setChunkedStreamingMode(0);
+                connection.setRequestMethod("POST");
+
+                Uri.Builder builder = new Uri.Builder()
+                        .appendQueryParameter("room", inRoom)
+                        .appendQueryParameter("imageType", inPath.substring(inPath.lastIndexOf(".") + 1))
+                        .appendQueryParameter("image", encodedData);
+                String query = builder.build().getEncodedQuery();
+
+                OutputStream os = connection.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(query);
+                writer.flush();
+                writer.close();
+                os.close();
+
+                Log.d("HostActivity", "ResponseCode: " + Integer.toString(connection.getResponseCode()));
+
+            } catch ( Exception ex ) {
+                ex.printStackTrace();
+
+            } finally {
+                if ( connection != null ) connection.disconnect();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            super.onPostExecute(unused);
+            Toast.makeText(inContext,"Image uploaded",Toast.LENGTH_SHORT).show();
+        }
+    }
+
     public static class SelectGridAdapter extends RecyclerView.Adapter<ImageViewHolder> {
 
         @NonNull
@@ -115,6 +205,7 @@ public class HostActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View view) {
                     Log.d("SelectActivity", "onClick:" + mImageFileNames[holder.getAdapterPosition()]);
+                    new ImageUploader(view.getContext(), "Room_Z4HG", imgFile.getPath()).execute();
                 }
             };
             holder.imageView.setOnClickListener(imageViewClickListener);
